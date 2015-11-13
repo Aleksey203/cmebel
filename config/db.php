133b1b7f82a665bc317a3234257af071dbb3b69a
Aleksey Berlinskiy <al.berlinskiy@ya.ru>
2015-11-13 19:15:11 +0300

    Добавление composer.lock  db.php в gitignore

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=cmebel;port=83;dbname=cmebel',
+    'dsn' => 'mysql:host=localhost;dbname=cmebel',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
